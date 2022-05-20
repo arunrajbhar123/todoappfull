06d7318873fdd51f50d468dfeb872d9e31981be3
arunrajbhar123 <arunrajbhar1998@gmail.com>
2022-05-20 17:51:15 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import TodoApp from './components/TodoApp.jsx'
+function App() {
+  return (
+    <div className="App">
+ <TodoApp/>
+    </div>
+  );
+}
+
+export default App;
